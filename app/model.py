b0e52a54e0e46fa84e668d86c91a45bcf97e73a9
Ot Vadim <v.otdelnov77@gmail.com>
2021-12-21 22:08:46 +0300

    i need help

---
+++ app/model.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from app import db
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(70), index = True, unique = True)
+
+    def __repr__(self):
+        return "<User {}>".format(self.email)
