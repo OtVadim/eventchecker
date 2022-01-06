1468b85ff0ef9f7a9f69801432cc88dad4afec97
Ot Vadim <v.otdelnov77@gmail.com>
2022-01-06 21:14:16 +0300

    add user model

--- learn_app/model.py
+++ learn_app/model.py
@@ -2,11 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from learn_app import db
 
-db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(70), index = True, unique = True)
+    password_hash = db.Column(db.String(128))
 
     def __repr__(self):
         return "<User {}>".format(self.email)
